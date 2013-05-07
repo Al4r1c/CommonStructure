133852d3c24d479b89bcb056eafb94f097291c56
yann <yann.kurdijaka@wanadoo.fr>
2013-05-07 10:13:45 +0200

    Initial Commit Base Structure

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+include_once(__DIR__ . '/config/bootstrap.php');
+
+include_once(__DIR__ . '/vendor/autoload.php');
+
+
+$config = array(
+    'configFile' => __DIR__ . '/config/config.yaml',
+    'routeFile' => __DIR__ . '/config/routes.yaml',
+    'controllersPath' => __DIR__ . '/src/application/controller/',
+    'templatesPath' => __DIR__ . '/src/application/templates/',
+    'localesPath' => __DIR__ . '/public/locale/',
+);
+
+$main = new \AlaroxFramework\Main($config);
+echo $main->run();
